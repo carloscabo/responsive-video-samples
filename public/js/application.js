495ffda5e0668792087f88526a53da46fa009327
carloscabo <carloscabo@the-cocktail.com>
2014-10-08 13:27:18 +0200

    Refreshing centering on window resize

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,26 @@
 // — FUNCIONES LOCALES A ESTE SITE
 $(document).ready(function() {
 	// la magia aquí
+  console.log('eo');
+
+  function v_center_videos() {
+    $('video.js-autocenter').each(function(index, el) {
+      var
+        pw = $(el).parent().outerWidth(),
+        ph = $(el).parent().outerHeight(),
+        ew = $(el).outerWidth(),
+        eh = $(el).outerHeight();
+
+      $(el).css({
+        'margin-top': ((eh/2)-(ph/2))*(-1),
+        'margin-left': ((ew/2)-(pw/2))*(-1)
+      });
+    });
+  }
+  v_center_videos();
+
+  $(window).resize(function() {
+    v_center_videos();
+  });
 
 });
