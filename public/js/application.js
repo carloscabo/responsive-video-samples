3a575cbaa8197a90dab321c40c2bcca6032fe9e1
carloscabo <carlos.cabo@the-cocktail.com>
2015-08-12 14:12:03 +0200

    Fixing z-index

--- public/js/application.js
+++ public/js/application.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
   // La magia aquí
   console.log('domready');
 
+  // Video.js settings
+  videojs("video-js").ready(function(){
+    myPlayer.muted(true);
+  });
 
-  // Autohide vídeo-js controls
-  setTimeout(function(){
-    // V('video-js').controlBar.fadeOut(); // or hide()
-  }, 2000);
+  // Autohide vídeo-js controls if fail
+  /*setTimeout(function(){
+    V('video-js').controlBar.fadeOut(); // or hide()
+  }, 2000);*/
 
 });
