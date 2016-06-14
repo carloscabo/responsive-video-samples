36b699147f8f8a71dfc4d5c937e36baeb9748bc9
carloscabo <carlos.cabo@the-cocktail.com>
2016-06-14 16:40:10 +0200

    Added Bideo.js sample

--- js/ready.js
+++ js/ready.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 
   // ///////////////////////////////////////////////////////////
   // Sample A
-  // If HTML5 video is supported nativelly
+  // ///////////////////////////////////////////////////////////
+
+  // Check if HTML5 video is supported nativelly
   if(!!document.createElement('video').canPlayType) {
     var
       $vsA = $('#video-sample-A');
@@ -31,7 +33,7 @@ $(document).ready(function() {
   // ///////////////////////////////////////////////////////////
   // Sample B
   // Video.js settings
-  var myPlayer = videojs("video-js").ready(function(){
+  var myPlayer = videojs('video-js').ready(function(){
     myPlayer.muted(true);
   });
   $(document)
@@ -54,6 +56,7 @@ $(document).ready(function() {
 
   // ///////////////////////////////////////////////////////////
   // Smaple C. Youtube IFrame API
+  // ///////////////////////////////////////////////////////////
 
   // Replace the 'ytplayer' element with an <iframe> and
   // YouTube player after the API code downloads.
@@ -86,11 +89,66 @@ $(document).ready(function() {
   };
 
   // Load the IFrame Player API code asynchronously.
+  // Once loadad it will call the onYouTubePlayerAPIReady() function.
   var tag = document.createElement('script');
   tag.src = "https://www.youtube.com/player_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+  // ///////////////////////////////////////////////////////////
+  // Smaple D. Full container BG with Bideo.js
+  // ///////////////////////////////////////////////////////////
+  window.bideo = new Bideo();
+  window.bideo.init({
+    // Video element
+    // videoEl: document.querySelector('#bideo-bg'),
+    videoEl: $('#bideo-bg')[0],
+    // Container element
+    container: document.querySelector('#bideo-container'),
+    // Resize
+    resize: true,
+    autoplay: true,
+    muted: true,
+    isMobile: window.matchMedia('(max-width: 768px)').matches,
 
+    // playButton: document.querySelector('#bideo-play'),
+    // pauseButton: document.querySelector('#bideo-pause'),
+
+    // Array of objects containing the src and type
+    // of different video formats to add
+    src: [
+      {
+        src: 'img/sample.mp4',
+        type: 'video/mp4'
+      },
+      {
+        src: 'sample.webm',
+        type: 'video/webm;codecs="vp8, vorbis"'
+      }
+    ],
+    onLoad: function () {
+      // What to do once video loads (initial frame)
+      // document.querySelector('#video_cover').style.display = 'none';
+      // We can use standard JS functionallity
+      $vsD = $('#bideo-bg');
+      $(document)
+        .on('click', '#bideo-play', function(e) {
+          e.preventDefault();
+          $vsD[0].play();
+        })
+        .on('click', '#bideo-pause', function(e) {
+          e.preventDefault();
+          $vsD[0].pause();
+        })
+        .on('click', '#bideo-mute', function(e) {
+          e.preventDefault();
+          if ( $vsD[0].muted ) {
+            $vsD.prop('muted', false); //unmute
+          } else {
+            $vsD.prop('muted', true); //mute
+          }
+        });
+    }
+  });
 
 });
