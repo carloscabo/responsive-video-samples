fe79a1066e4f854f649644597952e4012c0cea3c
carloscabo <carlos.cabo@the-cocktail.com>
2016-06-14 15:38:57 +0200

    Insertar vídeo con YouTube API

--- js/ready.js
+++ js/ready.js
@@ -91,24 +91,6 @@ $(document).ready(function() {
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // Autohide vídeo-js controls if fail
-  /*setTimeout(function(){
-    V('video-js').controlBar.fadeOut(); // or hide()
-  }, 2000);*/
 
-  // Ejemplo de poner vídeos de baja resolución en móviles
-  /*sources = {
-    mp4: '//ws-shop.s3.amazonaws.com/public/files/videos/ItsHEREitsNOW_OK_{{RES}}w.mp4',
-    webm: '//ws-shop.s3.amazonaws.com/public/files/videos/ItsHEREitsNOW_OK_{{RES}}w.webm'
-    },
-    res = ($('body').width() < 769) ? res = 640 : res = 1024,
-    $vjs = $('#video-js');
-  if (res === 640) {
-    $vjs.removeAttr('autoplay').removeAttr('muted');
-  }
-  $.each(sources, function(key, value) {
-    $('<source>').attr('type','video/'+key).attr('src', value.replace('{{RES}}', res)).appendTo($vjs);
-  });
-  */
 
 });
